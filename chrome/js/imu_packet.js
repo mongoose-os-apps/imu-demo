4ae3f47c0aa46580ca539d59199f810a4b673c21
Pim van Pelt <pim@google.com>
2018-12-08 16:02:21 -0800

    Fill in IMU* table based on STATS/INFO packets

--- chrome/js/imu_packet.js
+++ chrome/js/imu_packet.js
@@ -1,13 +1,27 @@
 "use strict";
 
 var imuData = {a: [0, 0, 0], g: [0, 0, 0], m: [0, 0, 0]};
+var imuInfo = {};
+var imuStats = {};
 
 function imu_packet_info(packet) {
-  console.log("INFO Packet:", packet);
+  try {
+    imuInfo = JSON.parse(packet);
+    $('div#hud td.imu_rate').text(imuInfo.imu_hz+'Hz')
+    $('div#hud td.imu_accel_type').text(imuInfo.accelerometer.type)
+    $('div#hud td.imu_gyro_type').text(imuInfo.gyroscope.type)
+    $('div#hud td.imu_mag_type').text(imuInfo.magnetometer.type)
+  } catch (ignore) {
+    console.log("Malformed INFO Packet: ", packet);
+  }
 }
 
 function imu_packet_stats(packet) {
-  console.log("STATS Packet:", packet);
+  try {
+    imuStats = JSON.parse(packet);
+  } catch (ignore) {
+    console.log("Malformed STATS Packet: ", packet);
+  }
 }
 
 function imu_packet_data(packet) {
