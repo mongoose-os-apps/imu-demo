306e8be03e462f3c8323572c6bba5b72b96f6ed8
Pim van Pelt <pim@google.com>
2018-12-11 21:51:53 +0100

    Depart from JSON output -- do a binary protocol now

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include "mgos.h"
 #include "mgos_i2c.h"
 #include "mgos_imu.h"
+#include "madgwick.h"
 
-#define IMU_HZ           100
-#define IMU_PACKET_STATS_PERIOD 1.00 // Seconds
-#define IMU_PACKET_INFO_PERIOD  5.00 // Seconds
-#define IMU_PACKET_ID_INFO  1
-#define IMU_PACKET_ID_STATS 2
-#define IMU_PACKET_ID_DATA  3
+#define IMU_HZ                     100
+#define IMU_PACKET_STATS_PERIOD    1000    // msec
+#define IMU_PACKET_INFO_PERIOD     5000    // msec
+
+struct mgos_imu_madgwick *s_filter = NULL;
 
 struct imu_packet {
   float ax, ay, az;
@@ -15,74 +15,102 @@ struct imu_packet {
   float mx, my, mz;
 };
 
-static void emit_imu_packet(const uint8_t id, const char *msg) {
-  printf("$P>%d:", id);
-  printf(msg);
-  printf("\r\n");
+static void emit_imu_packet_stats(void *user_data) {
+  static double   last_counter_emission = 0;
+  static uint32_t last_counter          = 0;
+  uint32_t        counter;
+  double          now;
+  float           freq;
+
+  mgos_imu_madgwick_get_counter(s_filter, &counter);
+  now  = mg_time();
+  freq = (counter - last_counter) / (now - last_counter_emission);
+
+  write(1, "$P>A", 3);
+  write(1, &counter, 4);
+  write(1, &freq, 4);
+
+  last_counter          = counter;
+  last_counter_emission = now;
 }
 
-static void emit_imu_packet_stats() {
-  char *msg;
+static void emit_imu_packet_info(void *user_data) {
+  struct mgos_imu *imu = (struct mgos_imu *)user_data;
+  const char *     a, *g, *m;
 
-  msg = json_asprintf("{}");
-  if (!msg)
-    return;
+  a = mgos_imu_accelerometer_get_name(imu);
+  g = mgos_imu_gyroscope_get_name(imu);
+  m = mgos_imu_magnetometer_get_name(imu);
 
-  emit_imu_packet(IMU_PACKET_ID_STATS, msg);
-  free (msg);
+  write(1, "$P>B", 3);
+  write(1, a, strlen(a));
+  write(1, ",", 1);
+  write(1, g, strlen(a));
+  write(1, ",", 1);
+  write(1, m, strlen(a));
+  return;
 }
 
 static void emit_imu_packet_data(struct imu_packet *p) {
-  char *msg;
-  if (!p) return;
-
-  msg = json_asprintf("{a: [%f, %f, %f], g: [%f, %f, %f], m: [%f, %f, %f]}",
-      p->ax, p->ay, p->az,
-      p->gx, p->gy, p->gz,
-      p->mx, p->my, p->mz);
-  if (!msg)
-    return;
-
-  emit_imu_packet(IMU_PACKET_ID_DATA, msg);
-  free (msg);
+  write(1, "$P>C", 3);
+  write(1, p, sizeof(struct imu_packet));
+  return;
 }
 
-static void emit_imu_packet_info(struct mgos_imu *imu) {
-  char *msg;
-  if (!imu) return;
-  msg = json_asprintf("{accelerometer: { type: \"%s\" }, gyroscope: { type: \"%s\" }, magnetometer: { type: \"%s\" }, imu_hz: %d}",
-      mgos_imu_accelerometer_get_name(imu),
-      mgos_imu_gyroscope_get_name(imu),
-      mgos_imu_magnetometer_get_name(imu),
-      IMU_HZ);
+static void emit_imu_packet_quat(struct mgos_imu_madgwick *filter) {
+  float q0, q1, q2, q3;
 
-  if (!msg) return;
-  emit_imu_packet(IMU_PACKET_ID_INFO, msg);
-  free(msg);
+  mgos_imu_madgwick_get_quaternion(filter, &q0, &q1, &q2, &q3);
+
+  write(1, "$P>D", 3);
+  write(1, &q0, 4);
+  write(1, &q1, 4);
+  write(1, &q2, 4);
+  write(1, &q3, 4);
+  return;
+}
+
+static void emit_imu_packet_angles(struct mgos_imu_madgwick *filter) {
+  float r, p, y;
+
+  mgos_imu_madgwick_get_angles(filter, &r, &p, &y);
+
+  write(1, "$P>D", 3);
+  write(1, &r, 4);
+  write(1, &p, 4);
+  write(1, &y, 4);
+  return;
 }
 
 static void imu_cb(void *user_data) {
-  static double last_info_emission = 0;
-  static double last_stats_emission = 0;
-  struct mgos_imu *imu = (struct mgos_imu *)user_data;
+  struct mgos_imu * imu = (struct mgos_imu *)user_data;
   struct imu_packet imu_packet;
 
-  if (!imu) return;
+  if (!imu) {
+    return;
+  }
 
   mgos_imu_accelerometer_get(imu, &imu_packet.ax, &imu_packet.ay, &imu_packet.az);
   mgos_imu_gyroscope_get(imu, &imu_packet.gx, &imu_packet.gy, &imu_packet.gz);
   mgos_imu_magnetometer_get(imu, &imu_packet.mx, &imu_packet.my, &imu_packet.mz);
 
-  emit_imu_packet_data(&imu_packet);
+  mgos_imu_madgwick_update(s_filter,
+                           imu_packet.gx,
+                           imu_packet.gy,
+                           imu_packet.gz,
+                           imu_packet.ax,
+                           imu_packet.ay,
+                           imu_packet.az,
+                           0,
+                           0,
+                           0);
+//      &imu_packet.mx,
+//      &imu_packet.my,
+//      &imu_packet.mz);
 
-  if (last_info_emission == 0 || (mg_time() - last_info_emission > IMU_PACKET_INFO_PERIOD)) {
-    emit_imu_packet_info(imu);
-    last_info_emission = mg_time();
-  }
-  if (last_stats_emission == 0 || (mg_time() - last_stats_emission > IMU_PACKET_STATS_PERIOD)) {
-    emit_imu_packet_stats();
-    last_stats_emission = mg_time();
-  }
+  emit_imu_packet_quat(s_filter);
+  emit_imu_packet_angles(s_filter);
+  emit_imu_packet_data(&imu_packet);
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
@@ -99,14 +127,23 @@ enum mgos_app_init_result mgos_app_init(void) {
     return false;
   }
 
-  if (!mgos_imu_accelerometer_create_i2c(imu, i2c, 0x68, ACC_MPU9250))
+  if (!mgos_imu_accelerometer_create_i2c(imu, i2c, 0x68, ACC_MPU9250)) {
     LOG(LL_ERROR, ("Cannot create accelerometer on IMU"));
-  if (!mgos_imu_gyroscope_create_i2c(imu, i2c, 0x68, GYRO_MPU9250))
+  }
+  if (!mgos_imu_gyroscope_create_i2c(imu, i2c, 0x68, GYRO_MPU9250)) {
     LOG(LL_ERROR, ("Cannot create gyroscope on IMU"));
-  if (!mgos_imu_magnetometer_create_i2c(imu, i2c, 0x0C, MAG_AK8963))
+  }
+  if (!mgos_imu_magnetometer_create_i2c(imu, i2c, 0x0C, MAG_AK8963)) {
     LOG(LL_ERROR, ("Cannot create magnetometer on IMU"));
+  }
 
+  if (!(s_filter = mgos_imu_madgwick_create())) {
+    LOG(LL_ERROR, ("Cannot create magnetometer on IMU"));
+  }
+
+  // Install timers
   mgos_set_timer(1000 / IMU_HZ, true, imu_cb, imu);
+  mgos_set_timer(IMU_PACKET_STATS_PERIOD, true, emit_imu_packet_stats, NULL);
+  mgos_set_timer(IMU_PACKET_INFO_PERIOD, true, emit_imu_packet_info, imu);
   return MGOS_APP_INIT_SUCCESS;
 }
-
