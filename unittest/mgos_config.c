69ad35146bb0745dda365faa4a4e48c68593c6c2
Pim van Pelt <pim@ipng.nl>
2019-01-05 00:25:59 +0100

    Remove unused mgos_config.*

--- unittest/mgos_config.c
+++
@@ -1,5 +0,0 @@
-#include "mgos_config.h"
-
-const char *mgos_sys_config_get_device_id() {
-  return "esp8266_ABCDEF";
-}
--- unittest/mgos_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MGOS_CONFIG_H
-#define __MGOS_CONFIG_H
-
-const char *mgos_sys_config_get_device_id();
-
-#endif // __MGOS_CONFIG_H
