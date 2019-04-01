dc29f885fe2d5d3f9e24ebeb116c0d17c1be86f2
Vasily Kiniv <vasily.kiniv@gmail.com>
2019-04-01 08:13:17 +0300

    Gently close serial connections on exit

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,8 +1,16 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        'bounds': {
-            'width': 1000,
+chrome.app.runtime.onLaunched.addListener(function () {
+  chrome.app.window.create('index.html', {
+    'bounds': {
+      'width': 1000,
       'height': 600
-        }
+    }
+  }, function (w) {
+    w.onClosed.addListener(function () {
+      chrome.serial.getConnections(function (connections) {
+        connections.forEach(function (c) {
+          chrome.serial.disconnect(c.connectionId, function () { });
+        });
+      });
     });
+  });
 });
